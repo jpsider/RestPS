589fc323da931769100fa4e099e9d2736149f4ce
jpsider <jpsider@gmail.com>
2018-05-22 20:12:46 -0400

    #17 -updated build script, fixed typo in .psd1

--- module.build.ps1
+++ module.build.ps1
@@ -82,7 +82,8 @@ Task BuildPSM1 -Inputs (Get-Item "$source\*\*.ps1") -Outputs $ModulePath {
 }
 
 Task NextPSGalleryVersion -if (-Not ( Test-Path "$output\version.xml" ) ) -Before BuildPSD1 {
-    $galleryVersion = Get-NextPSGalleryVersion -Name $ModuleName
+    #$galleryVersion = Get-NextPSGalleryVersion -Name $ModuleName
+    $galleryVersion = Get-NextNugetPackageVersion -Name $ModuleName
     $galleryVersion | Export-Clixml -Path "$output\version.xml"
 }
 

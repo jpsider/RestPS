c62e00d3d18b763e32af56208aa9578cd721d1eb
Justin Sider <jsider@belaytech.com>
2020-03-25 06:10:40 -0400

    updated tests for router file
    removed mock of write-log !deploy

--- tests/Private/Invoke-RequestRouter.Tests.ps1
+++ tests/Private/Invoke-RequestRouter.Tests.ps1
@@ -15,7 +15,6 @@ $Routes = $Routes
 $RoutesFilePath = "$env:systemdrive/RestPS/endpoints/routes.json"
 Function Import-RouteSet {}
 Function Invoke-Expression {}
-Function Write-Log {}
 Describe "Invoke-RequestRouter function for $script:ModuleName" -Tags Build {
     It "Should return Invalid Command, if invoke expression fails." {
         Mock -CommandName 'Import-RouteSet' -MockWith {}
@@ -31,9 +30,7 @@ Describe "Invoke-RequestRouter function for $script:ModuleName" -Tags Build {
             return $null
         }
         Mock -CommandName 'Set-Location' -MockWith {}
-        Mock -CommandName 'Write-Log' -MockWith {}
         Invoke-RequestRouter -RequestType "GET" -RequestURL "/proc" -RoutesFilePath $RoutesFilePath | Should be $null
-        Assert-MockCalled -CommandName 'Write-Log' -Times 3 -Exactly
     }
     It "Should return No Matching Routes, if the URL is invalid." {
         Mock -CommandName 'Import-RouteSet' -MockWith {
@@ -42,27 +39,21 @@ Describe "Invoke-RequestRouter function for $script:ModuleName" -Tags Build {
         Mock -CommandName 'Invoke-Expression' -MockWith {
             return $null
         }
-        Mock -CommandName 'Write-Log' -MockWith {}
         Invoke-RequestRouter -RequestType "GET" -RequestURL "/FakeURL" -RoutesFilePath $RoutesFilePath | Should be $null
-        Assert-MockCalled -CommandName 'Write-Log' -Times 5 -Exactly
     }
     It "Should return No Matching Routes, if the URL is invalid." {
         Mock -CommandName 'Invoke-Expression' -MockWith {
             return $null
         }
-        Mock -CommandName 'Write-Log' -MockWith {}
         Invoke-RequestRouter -RequestType "GET" -RequestURL "/FakeURL" -RoutesFilePath $RoutesFilePath 
         $script:StatusDescription | Should be "Not Found"
-        Assert-MockCalled -CommandName 'Write-Log' -Times 7 -Exactly
     }
     It "Should return No Matching Routes, if the URL is invalid." {
         Mock -CommandName 'Invoke-Expression' -MockWith {
             return $null
         }
-        Mock -CommandName 'Write-Log' -MockWith {}
         Invoke-RequestRouter -RequestType "GET" -RequestURL "/FakeURL" -RoutesFilePath $RoutesFilePath 
         $script:StatusCode | Should be 404
-        Assert-MockCalled -CommandName 'Write-Log' -Times 9 -Exactly
     }
     It "Should not be null, when routes are returned." {
         $tempDir = (Get-Location).Path
@@ -80,17 +71,13 @@ Describe "Invoke-RequestRouter function for $script:ModuleName" -Tags Build {
             return $null
         }
         Mock -CommandName 'Set-Location' -MockWith {}
-        Mock -CommandName 'Write-Log' -MockWith {}
         Invoke-RequestRouter -RequestType "GET" -RequestURL "/endpoint/routes" -RoutesFilePath $RoutesFilePath | Should not be $null
-        Assert-MockCalled -CommandName 'Write-Log' -Times 12 -Exactly
     }
     It "Should return True" {
         Mock -CommandName 'Invoke-Expression' -MockWith {
             return 'True @{ProcessName=calc.exe}'
         }
         Mock -CommandName 'Set-Location' -MockWith {}
-        Mock -CommandName 'Write-Log' -MockWith {}
         Invoke-RequestRouter -RequestType "GET" -RequestURL "/proc" -RoutesFilePath $RoutesFilePath | Should Be $true
-        Assert-MockCalled -CommandName 'Write-Log' -Times 15 -Exactly
     }
 }

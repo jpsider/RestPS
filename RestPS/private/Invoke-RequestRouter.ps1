5f500f312ac03ef161954b5ae48aa1569019ee02
Justin Sider <jsider@belaytech.com>
2020-03-25 06:06:59 -0400

    Removed logging
    #40 This was causing issues with responses from the Endpoint

--- RestPS/private/Invoke-RequestRouter.ps1
+++ RestPS/private/Invoke-RequestRouter.ps1
@@ -31,7 +31,7 @@ function Invoke-RequestRouter
         [Parameter()][String]$RoutesFilePath
     )
     # Import Routes each pass, to include new routes.
-    Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-RequestRouter: Importing RouteSet"
+    #Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-RequestRouter: Importing RouteSet"
     Import-RouteSet -RoutesFilePath $RoutesFilePath
     $Route = ($Routes | Where-Object {$_.RequestType -eq $RequestType -and $_.RequestURL -eq $RequestURL})
 
@@ -43,13 +43,13 @@ function Invoke-RequestRouter
         if ($RequestCommand -like "*.ps1")
         {
             # Execute Endpoint Script
-            Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-RequestRouter: Executing Endpoint Script."
+            #Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-RequestRouter: Executing Endpoint Script."
             $CommandReturn = . $RequestCommand -RequestArgs $RequestArgs -Body $script:Body
         }
         else
         {
             # Execute Endpoint Command (No body allowed.)
-            Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-RequestRouter: Executing Endpoint Command."
+            #Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-RequestRouter: Executing Endpoint Command."
             $Command = $RequestCommand + " " + $RequestArgs
             $CommandReturn = Invoke-Expression -Command "$Command" -ErrorAction SilentlyContinue
         }
@@ -57,14 +57,14 @@ function Invoke-RequestRouter
         if ($null -eq $CommandReturn)
         {
             # Not a valid response
-            Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-RequestRouter: Bad Request (400)."
+            #Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-RequestRouter: Bad Request (400)."
             $script:StatusDescription = "Bad Request"
             $script:StatusCode = 400
         }
         else
         {
             # Valid response
-            Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-RequestRouter: Valid Response (200)."
+            #Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-RequestRouter: Valid Response (200)."
             $script:result = $CommandReturn
             $script:StatusDescription = "OK"
             $script:StatusCode = 200
@@ -73,7 +73,7 @@ function Invoke-RequestRouter
     else
     {
         # No matching Routes
-        Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-RequestRouter: No Matching routes (404)."
+        #Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-RequestRouter: No Matching routes (404)."
         $script:StatusDescription = "Not Found"
         $script:StatusCode = 404
     }

e56f31d7a02f54c63e719fbe065e544f8157a783
accc703 <6taXRg4sgssTt2>
2020-05-01 20:29:46 +0800

    Rewrite code for Basic Authentication

--- RestPS/bin/Invoke-VerifyBasicAuth.ps1
+++ RestPS/bin/Invoke-VerifyBasicAuth.ps1
@@ -1,33 +1,46 @@
 function Invoke-VerifyBasicAuth
 {
-    # Basic Authentication
-    . $RestPSLocalRoot\bin\Get-RestUserAuth.ps1
-    $RestUserAuth = (Get-RestUserAuth).UserData
+	# Basic Authentication
+	. $RestPSLocalRoot\bin\Get-RestUserAuth.ps1
+	$RestUserAuth = (Get-RestUserAuth).UserData
 
-    if ($null -ne $RestUserAuth)
-    {
-        # Get the System AuthString for the Client Request
-        $UserToCheck = $script:Subject
-        $SystemAuthString = ($RestUserAuth | Where-Object { $_.UserName -eq "$UserToCheck" }).SystemAuthString
+	if ($null -ne $RestUserAuth)
+	{
+        
+        	# Get the AuthString from Client Headers
+        	$ClientHeaders = $script:Request.Headers
+        	$ClientHeadersAuth = $ClientHeaders.GetValues("Authorization")
+        	$AuthType, $AuthString = $ClientHeadersAuth.split(" ")
+        	Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-VerifyBasicAuth: Auth type is: $AuthType, AuthString is: $AuthString"
 
-        # Get the AuthString from Client Headers
-        $ClientHeaders = $script:Request.Headers
-        $ClientHeadersAuth = $ClientHeaders.GetValues("Authorization")
-        $AuthType, $AuthString = $ClientHeadersAuth.split(" ")
-        Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-VerifyBasicAuth: Auth type is: $AuthType, AuthString is: $AuthString"
+ 			if ($null -ne $AuthString)
+			{
 
-        if ($null -ne $AuthString)
-        {
-            if ($SystemAuthString -eq $AuthString)
-            {
-                Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-VerifyBasicAuth: Client Authorization type: $AuthType is Verified."
-                $script:VerifyStatus = $true
-            }
-            else
-            {
-                Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-VerifyBasicAuth: Client did not pass Authorization type: $AuthType."
-                $script:VerifyStatus = $false
-            }
+				# Decode the Authorization header
+				$DecodedAuthString = [Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($AuthString))
+
+				# Compare the decoded authorisation header to the user record in $RestUserAuth one by one
+				foreach ($AllowedUser in $RestUserAuth) {	
+					$AllowedUserName = $AllowedUser.UserName
+					$AllowedUserPassword = $AllowedUser.SystemAuthString
+
+					if ($DecodedAuthString -eq $AllowedUserName + ":" + $AllowedUserPassword)
+	 				{
+						Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-VerifyBasicAuth: Client Authorization type: $AuthType is Verified."
+						$script:VerifyStatus = $true
+						Break
+					}
+					else
+					{
+						$script:VerifyStatus = $false
+					}		
+				}
+
+			    # Checked all user records in $RestUserAuth and no record match
+			    If ($script:VerifyStatus -eq $false) {
+					Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-VerifyBasicAuth: Client did not pass Authorization type: $AuthType."
+            	}
+	    
         }
         else
         {

44da6fef5bcbad547e7a05302588705c5a3b5698
jpsider <jpsider@gmail.com>
2020-07-26 20:45:42 -0400

    New feature for response type added. !deploy

--- tests/Private/Invoke-StreamOutput.Tests.ps1
+++ tests/Private/Invoke-StreamOutput.Tests.ps1
@@ -4,7 +4,6 @@ $here = (Split-Path -Parent $MyInvocation.MyCommand.Path) -replace 'tests', "$sc
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
-
 #describe "Invoke-StreamOutput function for $script:ModuleName" -Tags Build {
 #describe "Invoke-StreamOutput function for $script:ModuleName" {
 #    It 'Should return Null if output is streamed' {

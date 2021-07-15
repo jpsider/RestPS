fab1821faaee1a0a07d6ca30facfa85a365fb021
cyberblob <paragon56@hotmail.com>
2021-07-15 14:08:32 -0500

    Create Invoke-RestRequesterAuth.ps1
    Adds IP filtering via Get-RestIPAuth.ps1

---
+++ RestPS/bin/Invoke-RestRequesterAuth.ps1
@@ -0,0 +1,29 @@
+function Invoke-RestRequesterAuth
+{
+
+    # IP Based Authentication
+    . c:\restps\bin\Get-RestIPAuth.ps1
+    $RestIPAuth = (Get-RestIPAuth).UserIP
+        
+    $RequesterIP = $script:Request.RemoteEndPoint
+
+    if ($RestIPAuth -ne $null)
+    {
+        $RequesterIP, $RequesterPort = $RequesterIP -split (":")
+        $RequesterStatus = 0
+        
+        $RequesterStatus = $RestIPAuth | Where-Object {($_.IP -eq "$RequesterIP")}
+        
+        if(($RequesterStatus | Measure-Object).Count -eq 1)
+        {
+            $script:VerifyStatus = $true
+        }
+        else
+        {
+            $script:VerifyStatus = $false
+            Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType TRACE -Message "Invoke-RestRequesterAuth: Failed Invalid IP"
+        }
+        Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType TRACE -Message "Invoke-RestRequesterAuth: Valid IP"
+    }
+    $script:VerifyStatus
+}

cb2837736c39954586c58dc8341bc8bcdf45e9aa
cyberblob <paragon56@hotmail.com>
2021-07-15 15:00:04 -0500

    Update Invoke-RestRequesterAuth.ps1
    Removed unnecessary white spaces.

--- RestPS/bin/Invoke-RestRequesterAuth.ps1
+++ RestPS/bin/Invoke-RestRequesterAuth.ps1
@@ -1,19 +1,17 @@
 function Invoke-RestRequesterAuth
 {
-
     # IP Based Authentication
     . c:\restps\bin\Get-RestIPAuth.ps1
     $RestIPAuth = (Get-RestIPAuth).UserIP
-        
+
     $RequesterIP = $script:Request.RemoteEndPoint
 
     if ($RestIPAuth -ne $null)
     {
         $RequesterIP, $RequesterPort = $RequesterIP -split (":")
-        $RequesterStatus = 0
-        
+
         $RequesterStatus = $RestIPAuth | Where-Object {($_.IP -eq "$RequesterIP")}
-        
+
         if(($RequesterStatus | Measure-Object).Count -eq 1)
         {
             $script:VerifyStatus = $true

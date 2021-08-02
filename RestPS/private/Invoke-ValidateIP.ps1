b527bf976908cfe09f1e4c0b620165b706701840
cyberblob <paragon56@hotmail.com>
2021-08-02 14:55:47 -0500

    Update Invoke-ValidateIP.ps1
    Fixed Param from String to Bool

--- RestPS/private/Invoke-ValidateIP.ps1
+++ RestPS/private/Invoke-ValidateIP.ps1
@@ -16,9 +16,9 @@ function Invoke-ValidateIP
     [OutputType([boolean])]
     param(
         [Parameter()][String]$RestPSLocalRoot = "c:\RestPS",
-        [Parameter()][String]$VerifyClientIP
+        [Parameter()][Bool]$VerifyClientIP
     )
-    if($VerifyClientIP -eq $true)
+    if ($VerifyClientIP -eq $true)
     {
         . c:\restps\bin\Get-RestIPAuth.ps1
         $RestIPAuth = (Get-RestIPAuth).UserIP
@@ -27,7 +27,7 @@ function Invoke-ValidateIP
         {
             $RequesterIP, $RequesterPort = $RequesterIP -split (":")
             $RequesterStatus = $RestIPAuth | Where-Object {($_.IP -eq "$RequesterIP")}
-            if(($RequesterStatus | Measure-Object).Count -eq 1)
+            if (($RequesterStatus | Measure-Object).Count -eq 1)
             {
                 Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType INFO -Message "Invoke-ValidateIP: Valid Client IP"
                 $script:VerifyStatus = $true

c237ddd18df57510a53f2cc63c14f75ba7a53081
matsmcp <47147353+matsmcp@users.noreply.github.com>
2024-08-13 15:18:55 +0200

    Adjusted spacing and whitespaces

--- RestPS/private/Invoke-StartListener.ps1
+++ RestPS/private/Invoke-StartListener.ps1
@@ -24,31 +24,31 @@ function Invoke-StartListener
     if ($SSLThumbprint)
     {
         if ($SSLThumbprint -eq "None"){
-	    Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType TRACE -Message "Invoke-StartListener: No SSL Thumbprint present"
+	        Write-Log -LogFile $Logfile -LogLevel $logLevel -MsgType TRACE -Message "Invoke-StartListener: No SSL Thumbprint present"
             $Prefix = "http://"
   	    }
-	else
-	   {
-	   # Verify the Certificate with the Specified Thumbprint is available.
-           $CertificateListCount = ((Get-ChildItem -Path Cert:\LocalMachine -Recurse | Where-Object {$_.Thumbprint -eq "$SSLThumbprint"}) | Measure-Object).Count
-           if ($CertificateListCount -ne 0)
-              {
-               # SSL Thumbprint present, enabling SSL
-               netsh http delete sslcert ipport=0.0.0.0:$Port
-               netsh http add sslcert ipport=0.0.0.0:$Port certhash=$SSLThumbprint "appid={$AppGuid}"
-               $Prefix = "https://"
-              }
+	    else
+        {
+	        # Verify the Certificate with the Specified Thumbprint is available.
+            $CertificateListCount = ((Get-ChildItem -Path Cert:\LocalMachine -Recurse | Where-Object {$_.Thumbprint -eq "$SSLThumbprint"}) | Measure-Object).Count
+            if ($CertificateListCount -ne 0)
+            {
+                # SSL Thumbprint present, enabling SSL
+                netsh http delete sslcert ipport=0.0.0.0:$Port
+                netsh http add sslcert ipport=0.0.0.0:$Port certhash=$SSLThumbprint "appid={$AppGuid}"
+                $Prefix = "https://"
+            }
             else
-               {
-               Throw "Invoke-StartListener: Could not find Matching Certificate in CertStore: Cert:\LocalMachine"
-	    }
+            {
+                Throw "Invoke-StartListener: Could not find Matching Certificate in CertStore: Cert:\LocalMachine"
+	        }
         }
     }
     else
     {
     	# parameter used but No SSL Thumbprint given
         Throw "Invoke-StartListener: the SSLThumbPrint was used but no Thumbprint was given"
-       
+
     }
     try
     {

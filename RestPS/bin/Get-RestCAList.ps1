207b9986fbb74bed81661e37f60afd359377b5ea
jpsider <jpsider@gmail.com>
2024-08-16 16:05:15 -0500

    Resolved test failures. !deploy

--- RestPS/bin/Get-RestCAList.ps1
+++ RestPS/bin/Get-RestCAList.ps1
@@ -2,15 +2,15 @@ function Get-RestCAList
 {
     # This Function represents a way to Gather a List of accepted CA:s.
     # it uses the value from OID 2.5.29.14 of the CA Certificate
-    
-    $CAList = @('abvvdgetgsdsgfgs','sdghjrehgfjfxdhjkgmc')
+
+    $CAList = @('abvvdgetgsdsgfgs', 'sdghjrehgfjfxdhjkgmc')
     $CAList
 }
 
-function check-certificate
+function Confirm-Certificate
 {
     #Set to true to enable certificate validation
     #$checkcertificate=$false
-    $checkcertificate=$true
+    $checkcertificate = $true
     $checkcertificate
 }
